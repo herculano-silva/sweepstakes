9195029eca03663835874061661ad892f651c5fa
Herculano Silva <contato.herculanosilva@gmail.com>
2022-08-17 16:45:19 -0300

    feat: autenticação

---
+++ resources/views/auth/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+</head>
+<body>
+    <div class="min-h-screen w-full flex flex-col items-center justify bg-gradiente-to-b from-primary-base to-primary-light">
+        <img src="{{ asset("img/logo.svg") }}" alt="" class="h-40">
+    </div>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          base: "#F44A33",
+          light: "#FF4D57",
+          dark: "#B71C1C",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
